e10c2df3f9b951324cc115f6d368118198fdc79c
Dev Arome <moses21429@gmail.com>
2024-06-28 07:07:06 +0100

    completed bottom nav

--- lib/features/dashboard/widgets/bottom_nav.dart
+++ lib/features/dashboard/widgets/bottom_nav.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
 
 class BottomNavigationWidget extends ConsumerStatefulWidget {
   const BottomNavigationWidget({super.key});
@@ -12,27 +13,74 @@ class BottomNavigationWidget extends ConsumerStatefulWidget {
       _BottomNavigationWidgetState();
 }
 
+const homeFilled =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14"><path fill="currentColor" fill-rule="evenodd" d="M.318 6.045A1 1 0 0 0 0 6.776V12.5A1.5 1.5 0 0 0 1.5 14H6v-3a1 1 0 1 1 2 0v3h4.5a1.5 1.5 0 0 0 1.5-1.5V6.776a1 1 0 0 0-.318-.731L7.325.12a.5.5 0 0 0-.65 0z" clip-rule="evenodd"/></svg>';
+const profileFilled =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M332.64 64.58C313.18 43.57 286 32 256 32c-30.16 0-57.43 11.5-76.8 32.38c-19.58 21.11-29.12 49.8-26.88 80.78C156.76 206.28 203.27 256 256 256s99.16-49.71 103.67-110.82c2.27-30.7-7.33-59.33-27.03-80.6M432 480H80a31 31 0 0 1-24.2-11.13c-6.5-7.77-9.12-18.38-7.18-29.11C57.06 392.94 83.4 353.61 124.8 326c36.78-24.51 83.37-38 131.2-38s94.42 13.5 131.2 38c41.4 27.6 67.74 66.93 76.18 113.75c1.94 10.73-.68 21.34-7.18 29.11A31 31 0 0 1 432 480"/></svg>';
+const profileOutlined =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M10 4.5a2 2 0 1 1-4 0a2 2 0 0 1 4 0m1.5 0a3.5 3.5 0 1 1-7 0a3.5 3.5 0 0 1 7 0m-9 8c0-.204.22-.809 1.32-1.459C4.838 10.44 6.32 10 8 10s3.162.44 4.18 1.041c1.1.65 1.32 1.255 1.32 1.459a1 1 0 0 1-1 1h-9a1 1 0 0 1-1-1m5.5-4c-3.85 0-7 2-7 4A2.5 2.5 0 0 0 3.5 15h9a2.5 2.5 0 0 0 2.5-2.5c0-2-3.15-4-7-4" clip-rule="evenodd"/></svg>';
+
 class _BottomNavigationWidgetState
     extends ConsumerState<BottomNavigationWidget> {
   @override
   Widget build(BuildContext context) {
     final position = ref.watch(dashboardControllerProvider);
 
-    return BottomNavigationBar(
-        unselectedItemColor: Colors.black26,
-        selectedItemColor: Colors.blue,
-        showUnselectedLabels: true,
-        elevation: 1,
-        currentIndex: position,
-        onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Feather.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Feather.search), label: 'Search'),
-          BottomNavigationBarItem(
-              icon: Icon(Ionicons.albums_outline), label: 'Library'),
-          BottomNavigationBarItem(
-              icon: Icon(Ionicons.person_outline), label: 'Profile'),
-        ]);
+    return BottomAppBar(
+      color: Colors.black,
+      shape: const CircularNotchedRectangle(),
+      elevation: 0,
+      notchMargin: 8,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () => _onTap(0),
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: const Iconify(
+                homeFilled,
+                color: Colors.white,
+              ),
+            ),
+            InkWell(
+              onTap: () => _onTap(1),
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                  border: const Border(
+                    left: BorderSide(color: Color(0xFF2400FF), width: 4),
+                    right: BorderSide(color: Color(0xFF2400FF), width: 4),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () => _onTap(2),
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: const Iconify(
+                profileFilled,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   void _onTap(int index) {
@@ -43,12 +91,9 @@ class _BottomNavigationWidgetState
         context.go('/');
         break;
       case 1:
-        context.go('/search');
+        context.go('/');
         break;
       case 2:
-        context.go('/library');
-        break;
-      case 3:
         context.go('/profile');
         break;
       default:
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flighterr/utils/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
